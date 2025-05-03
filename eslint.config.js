d37faf8898161d37f857bf4a42a309ba1214bf6c
jzr <2447102247@qq.com>
2025-05-03 14:39:14 +0800

    14字体图标引入及以前

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  
+  {
+    'vue/multi-word-component-names':0, //不再强制要求组件命名
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+])
