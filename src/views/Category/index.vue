91fd3fabe1e92c9cd3e6fce458bd393c9c123533
jzr <2447102247@qq.com>
2025-05-11 15:03:21 +0800

    按照业务逻辑拆分函数

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,31 +1,10 @@
 <script setup>
-import { getCategoryAPI } from '@/apis/category';
-import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
-import { getBannerAPI } from '@/apis/home';
+import GoodsItem from '../Home/components/GoodsItem.vue';
+import { useBanner } from './composables/useBanner';
+import { useCategory } from './composables/useCategory';
+const { bannerList } = useBanner()
+const { categoryData } = useCategory()
 
-//获取数据
-const categoryData = ref({})
-const route = useRoute()
-const getCategory = async () => {
-  const res = await getCategoryAPI(route.params.id)
-  categoryData.value = res.result
-}
-onMounted(() => getCategory())
-
-//获取banner
-
-const bannerList = ref([])
-
-const getBanner = async () => {
-  const res = await getBannerAPI({
-    distributionSite:'2'
-  })
-  console.log(res);
-  bannerList.value = res.result
-}
-
-onMounted(() => getBanner())
 </script>
 
 <template>
@@ -46,6 +25,25 @@ onMounted(() => getBanner())
           </el-carousel-item>
         </el-carousel>
       </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+          <ul>
+            <li v-for="i in categoryData.children" :key="i.id">
+              <RouterLink to="/">
+                <img :src="i.picture" />
+                <p>{{ i.name }}</p>
+              </RouterLink>
+            </li>
+          </ul>
+      </div>
+        <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+          <div class="head">
+            <h3>- {{ item.name }}-</h3>
+          </div>
+          <div class="body">
+            <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+          </div>
+        </div>
     </div>
   </div>
 </template>
