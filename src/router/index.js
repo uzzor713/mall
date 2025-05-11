7c2836eda3b44d1b194bd5e0d779daddb89a1a37
jzr <2447102247@qq.com>
2025-05-11 17:26:51 +0800

    二级分类完成

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
-
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +23,10 @@ const router = createRouter({
         {
           path:'category/:id',
           component:Category,
+        },
+        {
+          path:'category/sub/:id',
+          component:SubCategory,
         }
       ]
     },
@@ -31,6 +35,12 @@ const router = createRouter({
       component:Login
     }
   ],
+  //路由滚动行为定制
+  scrollBehavior(){
+    return {
+      top: 0
+    }
+  }
 })
 
 export default router
