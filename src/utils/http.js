40a1c58c09aaaf8f528fe6321d17ae098c56562c
jzr <2447102247@qq.com>
2025-07-31 16:11:30 +0800

    v1.1

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,7 +6,7 @@ import { useUserStore } from "@/stores/userStore";
 import router from "@/router";
 
 const httpInstance = axios.create({
-  baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  baseURL:'https://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 10000
 })
 
